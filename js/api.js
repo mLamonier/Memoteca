38fde0708ea7c1cfb7a0ab57ebf3a208de2da9fd
unknown <miguel2002lamonier@gmail.com>
2025-05-23 11:44:28 -0300

    att

--- js/api.js
+++ js/api.js
@@ -1,10 +1,30 @@
+// objeto javascript que está armazenando funções
 const api = {
     async buscarPensamentos(){
         try {
             // faz o fetch (requisição) ao servidor que criamos, retorna uma promise
+            // automaticamente um fetch já é um método GET, por isso não precisa colocar
             const response = await fetch('http://localhost:3000/pensamentos');
             // a resposta desta requisição armazenamos em uma variável para transformá-la em um objeto javascript
             // antes: json ->  depois: javascript
+            return await response.json();
+        } catch (error) {
+            console.error('Erro ao renderizar pensamentos:', error);
+            alert('Erro ao renderizar pensamento');
+        }
+    },
+
+    async salvarPensamento(pensamento){
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                // o "assunto" do método, que no caso é do tipo json
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                // como é enviado em formato JSON, precisamos converter para objeto javascript
+                body: JSON.stringify(pensamento)
+            });
             return await response.json();
         } catch (error) {
             console.error('Erro ao renderizar pensamentos:', error);
--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 // DOMContentLoaded = executa quando o HTML termina de ser carregado
 document.addEventListener('DOMContentLoaded', () => {
     //exibe os pensamentos que estão presentes no db.json no HTML
     ui.renderizarPensamentos();
+
+    const formularioPensamento = document.getElementById('pensamento-form');
+    //insere um evento de submissão de formulário onde quando executado,
+    //salva o novo pensamento no arquivo db.json, isso é uma requisição POST
+    formularioPensamento.addEventListener('submit', manipularSubmissaoFormulario);
+
+    const botaoCancelarNovoPensamento = document.getElementById('botao-cancelar');
+    botaoCancelarNovoPensamento.addEventListener('click', () => {
+        document.getElementById('pensamento-conteudo').value = '';
+        document.getElementById('pensamento-autoria').value = '';
+    })
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById('pensamento-id').value;
+    const conteudo = document.getElementById('pensamento-conteudo').value;
+    const autoria = document.getElementById('pensamento-autoria').value;
+
+    try{
+        await api.salvarPensamento({conteudo, autoria});
+        ui.renderizarPensamentos();
+    }catch (error){
+        alert('Erro ao salvar pensamento');
+    }
+}
